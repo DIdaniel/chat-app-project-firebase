071c477f7999c7a56dda9c4c87e38774a47d145d
DONG IL <cappu159@gmail.com>
2022-11-27 10:58:35 +0900

    changed background color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Home, Login, Register} from './components/pages';
 
 function App() {
   return (
-    <div className="flex justify-center items-center bg-blue-200 h-screen">
+    <div className="flex justify-center items-center bg-cyan-200 h-screen">
       <Home />
     </div>
   );
